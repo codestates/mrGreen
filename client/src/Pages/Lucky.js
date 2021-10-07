387d3a3da42c16e0bc7ff47066f25cffefc5c5da
shinseungmin-kor <shinseungmin10@gmail.com>
2021-10-07 12:32:14 +0900

    Add Components in Pages & basic css setup

---
+++ client/src/Pages/Lucky.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Lucky(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Lucky;
---
+++ client/src/Pages/PlantInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PlantInfo(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default PlantInfo;
