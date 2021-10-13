9cf906e7202d4201ea6409d745e97dea7dbe5555
seola25 <whatsa25@gmail.com>
2021-10-14 00:19:56 +0900

    [Client/Server] 로그인,사인업 모달 상태변경추가, 유저인포 겟요청 기능 구현

--- server/src/controllers/auth/login.js
+++ server/src/controllers/auth/login.js
@@ -1,3 +1,5 @@
+
+   
 const { user } = require("../../../models");
 require("dotenv").config();
 // const { sign, verify } = require("jsonwebtoken");
@@ -8,38 +10,41 @@ const {
   sendAccessToken,
 } = require("./tokenFunctions");
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   // 유효성 검사
-  //
-  // 401 { "message" : "Invalid password"}
-  // 404 {"message" : "Invalid user"}
-  // err
-  // 200 { data: { AccessToken: "string" }, message: "Login success" };
-
-  const regexForEmail =
-    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
-  const regexForPw = /(?=.*[a-zA-ZS])(?=.*?[#?!@$%^&*-]).{8,24}/;
+  // const regexForEmail =
+  //   /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+  // const regexForPw = /(?=.*[a-zA-ZS])(?=.*?[#?!@$%^&*-]).{8,24}/;
+  
+  // const isValidEmail = email.match(regexForEmail);
+  // const isValidPw = email.match(regexForPw);
+  // if (!isValidEmail) res.status(404).send({ message: "Invalid user" });
+  // if (!isValidPw) res.status(401).send({ message: "Invalid password" });
   const { email, password } = req.body;
-
-  const isValidEmail = email.match(regexForEmail);
-  const isValidPw = email.match(regexForPw);
-  if (!isValidEmail) res.status(404).send({ message: "Invalid user" });
-  if (!isValidPw) res.status(401).send({ message: "Invalid password" });
 
   user
     .findOne({
       where: { email: email, password: password },
     })
     .then((data) => {
-      //console.log("login server 컨트롤러", data)
+      //console.log("login server 컨트롤러", data);
       if (!data) {
         return res.status(404).send({ message: "Invalid user" });
+      } else {
+        console.log("login 파인드원 데이터 밸류",data.dataValues);
+        const userPayload = {
+          nickname: data.dataValues.nickname,
+          email: data.dataValues.email,
+        };
+        console.log(userPayload);
+        //console.log("login user payload ", userPayload);
+        // delete userPayload.password;
+        // console.log("login findOne password delete", userPayload)
+        const accessToken = generateAccessToken(userPayload);
+        const refreshToken = generateRefreshToken(userPayload);
+        sendRefreshToken(res, refreshToken);
+        sendAccessToken(res, accessToken);
       }
-      delete data.dataValues.password;
-      const accessToken = generateAccessToken(data.dataValues);
-      const refreshToken = generateRefreshToken(data.dataValues);
-      sendRefreshToken(res, refreshToken);
-      sendAccessToken(res, accessToken);
     })
     .catch((err) => console.log(err));
 };
